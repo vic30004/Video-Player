ea8d86dfb8269706e0c90b4ec0b03ba75d430ed3
vic30004 <victor_993@msn.com>
2020-03-30 20:10:58 -0700

    added evenlisteners

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+// Event Listeners
+
+video.addEventListener('click',toggleVideoStatus);
+video.addEventListener('pause',updatePlayIcon);
+video.addEventListener('play',updatePlayIcon);
+video.addEventListener('timeupdate',updateProgress);
+
+play.addEventListener('click',toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change',setVideoProgress)
