f0a31616ead4c2b5ce110c84175780843a2e4e48
vic30004 <victor_993@msn.com>
2020-03-30 20:13:39 -0700

    added the functions

--- app.js
+++ app.js
@@ -4,15 +4,40 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+// Play and Pause
+
+function toggleVideoStatus() {
+  return true;
+}
+
+function updatePlayIcon() {
+  return true;
+}
+
+// Update Progress and timestamp
+function updateProgress() {
+  return true;
+}
+
+function setVideoProgress() {
+  return true;
+}
+
+// Stop Video
+
+function stopVideo() {
+  return true;
+}
+
 // Event Listeners
 
-video.addEventListener('click',toggleVideoStatus);
-video.addEventListener('pause',updatePlayIcon);
-video.addEventListener('play',updatePlayIcon);
-video.addEventListener('timeupdate',updateProgress);
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
 
-play.addEventListener('click',toggleVideoStatus);
+play.addEventListener('click', toggleVideoStatus);
 
 stop.addEventListener('click', stopVideo);
 
-progress.addEventListener('change',setVideoProgress)
+progress.addEventListener('change', setVideoProgress);
