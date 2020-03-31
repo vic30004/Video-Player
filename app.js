067a7bcdc7e4e217cdeab0c8831f6efe96bf477e
vic30004 <victor_993@msn.com>
2020-03-30 20:17:25 -0700

    added logic to update the buttons

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ function toggleVideoStatus() {
 }
 
 function updatePlayIcon() {
-  return true;
+  if(video.paused){
+      play.innerHTML=`<i class="fa fa-play fa-2x"></i>`
+  }else{
+    play.innerHTML=`<i class="fa fa-pause fa-2x"></i>`
+  }
 }
 
 // Update Progress and timestamp
