6f44e82b03b5d26f3cd25a4e852a5b0f6495801e
vic30004 <victor_993@msn.com>
2020-03-30 20:24:10 -0700

    added logic to freely move the progress and update the video

--- app.js
+++ app.js
@@ -7,31 +7,31 @@ const timestamp = document.getElementById('timestamp');
 // Play and Pause
 
 function toggleVideoStatus() {
-  if(video.paused){
-      video.play();
-  }else{
-      video.pause();
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
   }
 }
 
 function updatePlayIcon() {
-  if(video.paused){
-      play.innerHTML=`<i class="fa fa-play fa-2x"></i>`
-  }else{
-    play.innerHTML=`<i class="fa fa-pause fa-2x"></i>`
+  if (video.paused) {
+    play.innerHTML = `<i class="fa fa-play fa-2x"></i>`;
+  } else {
+    play.innerHTML = `<i class="fa fa-pause fa-2x"></i>`;
   }
 }
 
 // Update Progress and timestamp
 function updateProgress() {
-  progress.value= (video.currentTime / video.duration) * 100;
-  if(video.currentTime === video.duration){
-      stopVideo()
+  progress.value = (video.currentTime / video.duration) * 100;
+  if (video.currentTime === video.duration) {
+    stopVideo();
   }
 }
 
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop Video
